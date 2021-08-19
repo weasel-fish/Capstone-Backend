4ccb0c3fa4eb657aa5c6e05027974e1611bd96f8
github username <github email address>
2021-08-19 14:31:43 -0400

    delete outing functionality

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -1,11 +1,11 @@
 class Outing < ApplicationRecord
     has_one_attached :image
 
-    has_many :user_outings
+    has_many :user_outings, dependent: :destroy
     has_many :users, through: :user_outings
-    has_many :sightings
+    has_many :sightings, dependent: :destroy
     has_many :animals, through: :sightings
-    has_many :outing_invites
+    has_many :outing_invites, dependent: :destroy
 
     validates :date, presence: true
     validates :name, presence: true
--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -3,7 +3,7 @@ class Sighting < ApplicationRecord
     
     belongs_to :outing
     belongs_to :animal
-    has_many :alerts
+    has_many :alerts, dependent: :destroy
 
     validates :outing_id, uniqueness: {scope: :animal_id}
 end
